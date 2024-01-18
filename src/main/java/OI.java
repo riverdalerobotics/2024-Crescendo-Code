dd12dcc4e5e86aa6e54fda856c7a8a9396914f4a
Galeteanu13 <nicolas.galeteanu@gmail.com>
2024-01-18 16:04:39 -0500

    First Commit o;ifhaswdfdilufhaewiuf

---
+++ src/main/java/OI.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+import edu.wpi.first.wpilibj.XboxController;
+
+/** Add your docs here. */
+public class OI {
+    XboxController moveController = new XboxController(0);
+    XboxController intakeController = new XboxController(1);
+//Movement controls
+public double xSpeed(){
+    return moveController.getLeftX();
+}
+
+public double ySpeed(){
+    return moveController.getLeftY();
+}
+
+public double rotate(){
+    return moveController.getRightX();
+}
+
+//Go to pickup position
+public boolean pickUpPos(){
+    return intakeController.getLeftBumper();
+}
+
+//Go to shoot position
+public boolean shootPos(){
+    return intakeController.getRightBumper();
+}
+
+//Shoot
+//TODO: make this a boton!! it is now in "testing mode"
+public double shoot(){
+    return intakeController.getRightTriggerAxis();
+}
+
+//Go to drivePos
+public boolean drivePos(){
+    return intakeController.getLeftStickButton();
+}
+
+// Arm Up and down
+public double moveArm(){
+    return intakeController.getLeftY();
+}
+//togle feild oriented
+public boolean togFeildOriented(){
+    return moveController.getBackButton();
+}
+//OTHER THINGS
+// STOP arm
+public boolean stopArm(){
+    return intakeController.getStartButton();
+}
+
+// Todao botton
+public boolean stopDrive(){
+    return moveController.getStartButton();
+}
+
+public boolean resetGyro(){
+    return intakeController.getBackButton();
+}
+
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.AutoShootCommand;
+import frc.robot.subsystems.IntakeSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  
+  public static final IntakeSubsystem INTAKE = new IntakeSubsystem();
+
+  // Create our inputs
+  XboxController intakeController = new XboxController(0);
+  XboxController moveController = new XboxController(1);
+ 
+
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  private void configureBindings() {
+      
+      }
+  
+   //TODO FIX THIS REMEMBER
+  public double getAutonomousCommand() {
+    //TODO: remove this
+      int x = 0;
+      return x;
+  }
+}
