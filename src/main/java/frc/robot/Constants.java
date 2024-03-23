ac08aff3d8918657ff6d7d8052d3c977c0e15191
heiress1 <aires.zheng1@gmail.com>
2024-03-23 10:42:59 -0400

    changed pid for pathplanner

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -336,7 +336,7 @@ public final class Constants {
 
 
   public static class PathPlannerConstants { //TODO: GET THESE CONSTANTS
-    public static final PIDConstants TRANSLATION_PID_CONSTANTS = new PIDConstants(3, 0, 0);
+    public static final PIDConstants TRANSLATION_PID_CONSTANTS = new PIDConstants(10, 0, 0);
     public static final PIDConstants ROTATION_PID_CONSTANTS = new PIDConstants(0.1, 0, 0);
 
     //TODO DETERMINE whether this is desired max or physical max
