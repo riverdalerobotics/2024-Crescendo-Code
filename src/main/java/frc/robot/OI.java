782bd78efb835a1ef35a2d4686c6aee75e219d46
BrandonR3P2 <branho19800@gmail.com>
2024-04-10 00:03:54 -0400

    added method to get class name from limelight detector

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Constants.OperatorConstants;
 
